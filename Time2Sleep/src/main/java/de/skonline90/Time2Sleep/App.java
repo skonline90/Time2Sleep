acad7f58207e78b8ae191e05c627ce74b7f94f89
skonl <skonl@10.77.211.20>
2018-07-27 16:19:35 +0200

    Created Project Structure

---
+++ Time2Sleep/src/main/java/de/skonline90/Time2Sleep/App.java
@@ -0,0 +1,13 @@
+package de.skonline90.Time2Sleep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
